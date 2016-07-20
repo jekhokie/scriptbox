72ac7a3f702b5e19a5b776edb83abb6edf772c54
Justin Karimi <jekhokie@gmail.com>
2016-07-20 13:12:18 +0000

    Wrap Injector Code in Infinite Loop
    Wrap the injector code in an infinite loop since we essentially want the
    injector to run indefinitely (or at least until the user terminates the
    injector).

--- python--web-service-simulator/run.py
+++ python--web-service-simulator/run.py
@@ -6,22 +6,23 @@ import time
 
 # check if we are an injector - if so, we only need to fire off a request and terminate
 if app.config['ROLE'] == 'Injector':
-  # create a random transaction ID
-  transaction_id = randint(100, 999)
+  while True:
+    # create a random transaction ID
+    transaction_id = randint(1, 1000000)
 
-  # simulate being the first service in a pipeline to receive a request
-  app.logger.info('Received Request', extra={'event': 'RECEIVED', 'transaction_id': transaction_id})
+    # simulate being the first service in a pipeline to receive a request
+    app.logger.info('Received Request', extra={'event': 'RECEIVED', 'transaction_id': transaction_id})
 
-  # inject an artificial delay between 100-300ms into the cycle to simulate "processing"
-  ms_delay = (randint(100, 300) / 1000.0)
-  time.sleep(ms_delay)
+    # inject an artificial delay between 100-300ms into the cycle to simulate "processing"
+    ms_delay = (randint(100, 300) / 1000.0)
+    time.sleep(ms_delay)
 
-  # record completing and passing the request downstream
-  app.logger.info('Processed Request', extra={'event': 'PROCESSED', 'transaction_id': transaction_id})
+    # record completing and passing the request downstream
+    app.logger.info('Processed Request', extra={'event': 'PROCESSED', 'transaction_id': transaction_id})
 
-  # send the transaction to the next service processor
-  payload = {'transaction_id': transaction_id}
-  requests.post(app.config['NEXT_HOP'], data=payload)
+    # send the transaction to the next service processor
+    payload = {'transaction_id': transaction_id}
+    requests.post(app.config['NEXT_HOP'], data=payload)
 else:
   # else, start as a processor or terminator
   app.run(host='0.0.0.0', port=app.config['PORT'], debug=app.config['DEBUG'])
