7d5142661c8744b4d68fbc77e1e40f58df611a09
Justin Karimi <jekhokie@gmail.com>
2016-07-20 12:43:20 +0000

    Add Logging for Transaction ID
    Add configuration to handle creating log files that include the
    transaction ID of the request. Also, include an example of how to log
    the extra parameters to the log file handler.

--- python--web-service-simulator/app/__init__.py
+++ python--web-service-simulator/app/__init__.py
@@ -1,14 +1,35 @@
 from flask import Flask
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 # construct the application object and apply configurations to the environment
 # setting "instance_relative_config=True" loads the configuration from the 'instance' folder
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 
+# set up logging
+app.logger.setLevel(logging.INFO)
+
+# configuration validation
 # make sure that the role specified for this instance is something that is recognized
 valid_roles = ['Injector', 'Processor', 'Terminator']
 if app.config['ROLE'] not in valid_roles:
   raise ValueError("The role specified in the configuration '{}' is not valid - must be one of '{}'".format(app.config['ROLE'], valid_roles))
+
+# create a service log directory if one does not yet exist
+# this is useful if you wish to run multiple services on the same host
+log_directory = "logs/{}".format(app.config['LISTEN_PATH'])
+if not os.path.exists(log_directory):
+  os.makedirs(log_directory)
+
+# set up the custom log to record transaction IDs
+log_file = "{}/{}.log".format(log_directory, app.config['LISTEN_PATH'])
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - Trans. ID [%(transaction_id)s] - %(message)s")
+handler = RotatingFileHandler(log_file, maxBytes=10000, backupCount=20)
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
 
 # load routes/views
 from app import views
--- python--web-service-simulator/app/views.py
+++ python--web-service-simulator/app/views.py
@@ -3,4 +3,5 @@ from app import app
 # default path
 @app.route("/{}".format(app.config['LISTEN_PATH']), methods=['GET', 'POST'])
 def handler():
+  app.logger.info('Received Request', extra={"transaction_id": "123"})
   return 'HERE', 201
