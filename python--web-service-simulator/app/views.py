6ca3c6b8cba95ccb3c81f65999dd465e57003e08
Justin Karimi <jekhokie@gmail.com>
2016-07-20 12:50:03 +0000

    Add Logging for Received and Processed, and Delay
    Add an artificial random delay to the processing of the incoming
    request, as well as log the received and processed events.

--- python--web-service-simulator/app/views.py
+++ python--web-service-simulator/app/views.py
@@ -1,7 +1,18 @@
 from app import app
+from random import randint
+import time
 
 # default path
-@app.route("/{}".format(app.config['LISTEN_PATH']), methods=['GET', 'POST'])
+@app.route("/{}".format(app.config['LISTEN_PATH']), methods=['POST'])
 def handler():
-  app.logger.info('Received Request', extra={"transaction_id": "123"})
+  # record receiving the request
+  app.logger.info('Received Request', extra={"event": "RECEIVED", "transaction_id": "123"})
+
+  # inject an artificial delay between 100-300ms into the cycle to simulate "processing"
+  ms_delay = (randint(100, 300) / 1000.0)
+  time.sleep(ms_delay)
+
+  # record completing and passing the request downstream
+  app.logger.info('Processed Request', extra={"event": "PROCESSED", "transaction_id": "123"})
+
   return 'HERE', 201
