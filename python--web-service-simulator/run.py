bf2e833aa49e7359dfbc1663226e3cbc5b45bcf7
Justin Karimi <jekhokie@gmail.com>
2016-07-20 13:11:00 +0000

    Add Injector Code
    Add injector code that only forwards a transaction request without
    actually starting a flask application.

--- python--web-service-simulator/run.py
+++ python--web-service-simulator/run.py
@@ -1,3 +1,27 @@
 # start the application after importing all relative app components
 from app import app
-app.run(host='0.0.0.0', port=app.config['PORT'], debug=app.config['DEBUG'])
+from random import randint
+import requests
+import time
+
+# check if we are an injector - if so, we only need to fire off a request and terminate
+if app.config['ROLE'] == 'Injector':
+  # create a random transaction ID
+  transaction_id = randint(100, 999)
+
+  # simulate being the first service in a pipeline to receive a request
+  app.logger.info('Received Request', extra={'event': 'RECEIVED', 'transaction_id': transaction_id})
+
+  # inject an artificial delay between 100-300ms into the cycle to simulate "processing"
+  ms_delay = (randint(100, 300) / 1000.0)
+  time.sleep(ms_delay)
+
+  # record completing and passing the request downstream
+  app.logger.info('Processed Request', extra={'event': 'PROCESSED', 'transaction_id': transaction_id})
+
+  # send the transaction to the next service processor
+  payload = {'transaction_id': transaction_id}
+  requests.post(app.config['NEXT_HOP'], data=payload)
+else:
+  # else, start as a processor or terminator
+  app.run(host='0.0.0.0', port=app.config['PORT'], debug=app.config['DEBUG'])
