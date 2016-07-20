96adb0088ae99af035cd5ebf2fc2b799c807acdf
Justin Karimi <jekhokie@gmail.com>
2016-07-20 13:10:37 +0000

    Update Code for Processor to Forward Request
    Update the code base to ensure that processors can forward requests as
    expected.

--- python--web-service-simulator/app/views.py
+++ python--web-service-simulator/app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import request
 from random import randint
+import requests
 import time
 
 # default path
@@ -10,13 +11,19 @@ def handler():
   transaction_id = request.form['transaction_id']
 
   # record receiving the request
-  app.logger.info('Received Request', extra={"event": "RECEIVED", "transaction_id": transaction_id})
+  app.logger.info('Received Request', extra={'event': 'RECEIVED', 'transaction_id': transaction_id})
 
   # inject an artificial delay between 100-300ms into the cycle to simulate "processing"
   ms_delay = (randint(100, 300) / 1000.0)
   time.sleep(ms_delay)
 
   # record completing and passing the request downstream
-  app.logger.info('Processed Request', extra={"event": "PROCESSED", "transaction_id": transaction_id})
+  app.logger.info('Processed Request', extra={'event': 'PROCESSED', 'transaction_id': transaction_id})
+
+  # if we are a processor, forward the transaction to the next service processor in line
+  # we don't care about a response for the purposes of this example application - ignore it
+  if app.config['ROLE'] == 'Processor':
+    payload = {'transaction_id': transaction_id}
+    requests.post(app.config['NEXT_HOP'], data=payload)
 
   return "TRANSACTION ID '{}' PROCESSED".format(transaction_id), 201
